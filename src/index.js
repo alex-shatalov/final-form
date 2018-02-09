907119bbe9f4f32ec53b32d94c10d94db63bb8d6
a.shatalov <a.shatalov@rambler-co.ru>
2018-02-09 11:50:52 +0300

    build

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// @flow
-export { default as createForm, FORM_ERROR, version } from './FinalForm'
-export { default as formSubscriptionItems } from './formSubscriptionItems'
-export { default as fieldSubscriptionItems } from './fieldSubscriptionItems'
-export { default as getIn } from './structure/getIn'
-export { default as setIn } from './structure/setIn'
